413c2786c7ce51d2b15460ee6177ad4bf0694da9
misha <mishavkrpv@gmail.com>
2024-04-28 18:39:01 +0300

    Rescaffold pages from origin service

---
+++ src/Identity.Web.Origin/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Identity.Web.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Web.Data;
+
+public sealed class ApplicationDbContext : IdentityDbContext<ApplicationsUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
