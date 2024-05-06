f64a37ecea62b2d967b3802016bfb7ae495aaaff
misha <mishavkrpv@gmail.com>
2024-05-06 06:03:41 +0300

    Add middleware extension for catching api requests

---
+++ src/Refleter.ServiceDefaults/MiddlewarePipelineExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Refleter.ServiceDefaults;
+
+public static class MiddlewarePipelineExtensions
+{
+    public static bool IsApiRequest(this HttpContext context)
+    {
+        return context.Request.Path.StartsWithSegments("/api", StringComparison.OrdinalIgnoreCase)
+               || context.Request.Headers["X-Requested-With"] == "XMLHttpRequest";
+    }
+}
