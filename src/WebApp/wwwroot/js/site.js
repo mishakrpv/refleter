9317dda75e6328376439bbf04c16d70347bece7d
misha <mishavkrpv@gmail.com>
2024-05-09 10:38:49 +0300

    Add create scope button within actions popup

--- src/WebApp/wwwroot/js/site.js
+++ src/WebApp/wwwroot/js/site.js
@@ -1,12 +1,37 @@
 ﻿const selectBtn = document.querySelector('#selectBtn')
-    
+const actionsBtn = document.querySelector('#actionsBtn')
+const actionsPopup = document.querySelector('#actionsPopup')
+
+const toggled = 'toggled'
+
 let toggleSelectBtn = () => {
-    let key = 'toggled'
-    if (selectBtn.classList.contains(key)) {
-        selectBtn.classList.remove(key)
+    if (selectBtn.classList.contains(toggled)) {
+        selectBtn.classList.remove(toggled)
     } else {
-        selectBtn.classList.add(key)
+        selectBtn.classList.add(toggled)
     }
 }
 
 selectBtn.onclick = toggleSelectBtn
+
+let showActionsPopup = () => {
+    if (!actionsPopup.classList.contains(toggled)) {
+        actionsPopup.classList.add(toggled)
+    }
+}
+
+actionsBtn.onclick = showActionsPopup
+
+document.addEventListener("click", function(event) {
+    if (event.target !== actionsBtn && !actionsBtn.contains(event.target)
+        && event.target !== actionsPopup && !actionsPopup.contains(event.target)) {
+        actionsPopup.classList.remove(toggled);
+    }
+});
+
+const createScopeBtn = document.querySelector('#createScopeBtn')
+const createScopeDialog = document.querySelector('#createScopeDialog')
+
+createScopeBtn.onclick = () => {
+    createScopeDialog.showModal()
+}
