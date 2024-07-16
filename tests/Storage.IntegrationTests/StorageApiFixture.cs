9c8c72f6de539527502b754442725041112baf3e
mishakrpv <mishavkrpv@gmail.com>
2024-07-16 20:37:36 +0300

    Add basics for integration testing of API services

---
+++ tests/Storage.IntegrationTests/StorageApiFixture.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Storage.IntegrationTests;
+
+public sealed class StorageApiFixture : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly IHost _app;
+    
+    public IResourceBuilder<PostgresServerResource> Postgres { get; private set; }
+    private string _postgresConnectionString;
+
+    public StorageApiFixture()
+    {
+        var options = new DistributedApplicationOptions
+        {
+            AssemblyName = typeof(StorageApiFixture).Assembly.FullName,
+            DisableDashboard = true
+        };
+
+        var appBuilder = DistributedApplication.CreateBuilder(options);
+
+        Postgres = appBuilder.AddPostgres("storagedb");
+        _app = appBuilder.Build();
+    }
+    
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureHostConfiguration(config =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                { $"ConnectionStrings:{Postgres.Resource.Name}", _postgresConnectionString },
+            }!);
+        });
+        return base.CreateHost(builder);
+    }
+    
+    public async Task InitializeAsync()
+    {
+        await _app.StartAsync();
+        _postgresConnectionString = await Postgres.Resource.GetConnectionStringAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await base.DisposeAsync();
+        await _app.StopAsync();
+        if (_app is IAsyncDisposable asyncDisposable)
+        {
+            await asyncDisposable.DisposeAsync().ConfigureAwait(false);
+        }
+        else
+        {
+            _app.Dispose();
+        }
+    }
+}
---
+++ tests/Storage.IntegrationTests/StorageApiTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using Asp.Versioning;
+using Asp.Versioning.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Storage.IntegrationTests;
+
+public sealed class StorageApiTests : IClassFixture<StorageApiFixture>
+{
+    private readonly WebApplicationFactory<Program> _webApplicationFactory;
+    private readonly HttpClient _httpClient;
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web);
+
+    public StorageApiTests(StorageApiFixture fixture)
+    {
+        var handler = new ApiVersionHandler(new QueryStringApiVersionWriter(), new ApiVersion(1.0));
+
+        _webApplicationFactory = fixture;
+        _httpClient = _webApplicationFactory.CreateDefaultClient(handler);
+    }
+}
